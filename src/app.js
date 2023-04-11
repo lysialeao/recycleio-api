cd3ca33957a5e30d151bba9c9ed92832b7ad6fd1
lysia <lysia@evnts.com.br>
2023-04-11 19:45:06 -0300

    feat: :zap: configure express to use body-parser instead of JSON.stringify for parsing JSON data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = require('./router')
 
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json())
 
 app.use(router)
 
-module.exports = app;
+module.exports = app
