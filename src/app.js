b56206252dac3bd967148a4783c75523c65c95b0
lysia <lysia@evnts.com.br>
2023-05-13 11:24:57 -0300

    feat: :zap: enabling CORS in the application

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./router')
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
 const app = express()
+
+app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 
