1ad668e9a9e865db7a58844be13a121dab595027
lysialeao <lysialeao@gmail.com>
2025-04-14 21:58:15 -0300

    feat:

--- src/models/collectionPointsModel.js
+++ src/models/collectionPointsModel.js
@@ -1,47 +1,55 @@
-const connection = require('./connection')
+const connection = require("./connection");
 
-const addressModel = require('./addressModel')
+const addressModel = require("./addressModel");
 
-const { defaultStatus }  = require('../enum/defaultStatus')
+const { defaultStatus } = require("../enum/defaultStatus");
 
-const { cryptoFunction } = require('../helpers/crypto')
+const { cryptoFunction } = require("../helpers/crypto");
 
 const getAll = async () => {
   try {
-      const [points] = await connection.execute('SELECT * FROM collection_points');
+    const [points] = await connection.execute(
+      "SELECT * FROM collection_points"
+    );
 
-      const promises = points.map(async (point) => {
-          const [waste] = await connection.execute(`
+    const promises = points.map(async (point) => {
+      const [waste] = await connection.execute(
+        `
               SELECT waste.name
               FROM waste_status
               JOIN waste ON waste_status.waste_id = waste.id
-              WHERE collection_point_id = ? AND waste_status.status = 'ACTIVE'`, [point.cnpj]);
+              WHERE collection_point_id = ? AND waste_status.status = 'ACTIVE'`,
+        [point.cnpj]
+      );
 
-          return {
-              ...point,
-              wastes: waste
-          };
-      });
-      const results = await Promise.all(promises);
-      return results;
+      return {
+        ...point,
+        wastes: waste,
+      };
+    });
+    const results = await Promise.all(promises);
+    return results;
   } catch (error) {
-      console.error('Erro ao executar a consulta:', error);
-      throw error;
+    console.error("Erro ao executar a consulta:", error);
+    throw error;
   }
 };
 
-
 const getCollectionPoint = async ({ login, password }) => {
-  const [point] = await connection.execute(`SELECT * FROM collection_points WHERE email='${login}' AND password='${password}'`)
-  return point
-}
+  const [point] = await connection.execute(
+    `SELECT * FROM collection_points WHERE email='${login}' AND password='${password}'`
+  );
+  return point;
+};
 
 const insertCollectionPoint = async (point) => {
+  const { address } = point;
 
-  const { address } = point
-
-  const insertedAddress = await addressModel.insertAddress(address)
-    .then(({ insertId }) => { return insertId })
+  const insertedAddress = await addressModel
+    .insertAddress(address)
+    .then(({ insertId }) => {
+      return insertId;
+    });
 
   const {
     cnpj,
@@ -52,64 +60,76 @@ const insertCollectionPoint = async (point) => {
     collection_days,
     collection_responsible_name,
     collection_responsible_email,
-    password
-  } = point
+    password,
+  } = point;
 
-  const cnpjInt = parseInt(cnpj)
-  const cipherpwd = cryptoFunction(password)
+  const cnpjInt = parseInt(cnpj);
+  const cipherpwd = cryptoFunction(password);
 
   const query = `INSERT INTO collection_points(cnpj, corporate_name, trade_name, telephone, email, collection_days, collection_responsible_name, collection_responsible_email, address_id, status, password)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-  const [insertedPoint] = await connection.query(query, [cnpjInt, corporate_name, trade_name, telephone, email, collection_days, collection_responsible_name, collection_responsible_email, insertedAddress, defaultStatus.active, cipherpwd])
+  const [insertedPoint] = await connection.query(query, [
+    cnpjInt,
+    corporate_name,
+    trade_name,
+    telephone,
+    email,
+    collection_days,
+    collection_responsible_name,
+    collection_responsible_email,
+    insertedAddress,
+    defaultStatus.active,
+    cipherpwd,
+  ]);
 
-  return insertedPoint
-
-}
+  return insertedPoint;
+};
 
 const getCollectionPointByWastes = async ({ wastes }) => {
-  const wasteIds = wastes.join(', '); // Transforma o vetor em uma string separada por vírgulas
+  const wasteIds = wastes.join(", "); // Transforma o vetor em uma string separada por vírgulas
 
   const query = `SELECT collection_point_id FROM waste_status WHERE waste_id IN (${wasteIds}) AND status='${defaultStatus.active}'`;
 
   const [wastesToFilter] = await connection.execute(query);
 
-  if(wastesToFilter.length >= 1 ) {
-    const collectionPointIds = wastesToFilter.map(waste => waste.collection_point_id);
-    const collectionPointIdsString = collectionPointIds.join(', ');
-  
+  if (wastesToFilter.length >= 1) {
+    const collectionPointIds = wastesToFilter.map(
+      (waste) => waste.collection_point_id
+    );
+    const collectionPointIdsString = collectionPointIds.join(", ");
+
     const secondQuery = `SELECT * FROM collection_points WHERE cnpj IN (${collectionPointIdsString});`;
-  
+
     const [points] = await connection.execute(secondQuery);
 
     const promises = points.map(async (point) => {
-      const [waste] = await connection.execute(`
+      const [waste] = await connection.execute(
+        `
           SELECT waste.name
           FROM waste_status
           JOIN waste ON waste_status.waste_id = waste.id
-          WHERE collection_point_id = ? AND waste_status.status = 'ACTIVE'`, [point.cnpj]);
+          WHERE collection_point_id = ? AND waste_status.status = 'ACTIVE'`,
+        [point.cnpj]
+      );
 
       return {
-          ...point,
-          wastes: waste
+        ...point,
+        wastes: waste,
       };
     });
     const results = await Promise.all(promises);
     return results;
-
-
   } else {
-    return []
+    return [];
   }
-
-
-}
+};
 
 const deleteCollectionPoint = async (cnpj) => {
-  const query = `UPDATE collection_points SET status='${defaultStatus.inactive}' WHERE cnpj='${cnpj}'`
-  const deletedPoint = await connection.query(query)
-  return deletedPoint
-}
+  const query = `UPDATE collection_points SET status='${defaultStatus.inactive}' WHERE cnpj='${cnpj}'`;
+  const deletedPoint = await connection.query(query);
+  return deletedPoint;
+};
 
 const getCollectionPointByCnpj = async (cnpj) => {
   const [point] = await connection.execute(` SELECT
@@ -122,18 +142,17 @@ const getCollectionPointByCnpj = async (cnpj) => {
     JSON_ARRAYAGG(
         JSON_OBJECT(
             'id', waste.id,
-            'name', waste.name,
-            'status': waste.status
+            'name', waste.name
         )
     ) AS waste_details
     FROM collection_points
     INNER JOIN address ON collection_points.address_id = address.id
-    LEFT JOIN waste_status ON collection_points.cnpj = waste_status.collection_point_id
+    LEFT JOIN waste_status ON collection_points.cnpj = waste_status.collection_point_id  AND waste_status.status = 'ACTIVE'
     LEFT JOIN waste ON waste_status.waste_id = waste.id
     WHERE collection_points.cnpj=${cnpj}
-    GROUP BY collection_points.cnpj, address.id; `)
-    return point
-}
+    GROUP BY collection_points.cnpj, address.id; `);
+  return point;
+};
 
 module.exports = {
   getAll,
@@ -141,5 +160,5 @@ module.exports = {
   getCollectionPointByWastes,
   deleteCollectionPoint,
   getCollectionPoint,
-  getCollectionPointByCnpj
-}
+  getCollectionPointByCnpj,
+};
